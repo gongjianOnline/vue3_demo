c1524dbd0d815915eef2a211b2326beae345d6ac
“李承斌” <15031617367@163.com>
2023-05-04 14:36:39 +0800

    watch 和 watchEffect使用

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,29 @@
 <template>
-  <div class="wrapper">
-    <table>
-      <thead>
-        <tr>
-          <td>商品</td>
-          <td>价格</td>
-          <td>单价</td>
-          <td>数量</td>
-          <td>操作</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(key,index) in goodList" :key="index">
-          <td>{{key.name}}</td>
-          <td>{{ key.unitPrice }}</td>
-          <td>{{ key.unitPrice*key.num }}</td>
-          <td>{{ key.num }}</td>
-          <td>
-            <button @click="handelClick(key,true)">+</button>
-            <button @click="handelClick(key,false)">-</button>
-          </td>
-        </tr>
-      </tbody>
-      <tfoot>
-        <tr>
-          <td>合计</td>
-          <td colspan="4">{{allPrice}}</td>
-        </tr>
-      </tfoot>
-    </table>
-  </div>
-</template>
-<script setup lang="ts">
-import {reactive,computed} from "vue";
-type Good = {
-  name:string,
-  num:number,
-  unitPrice:number
-}
-const goodList = reactive([
-  {
-    name:'苹果',
-    num:0,
-    unitPrice:55,
-  },
-  {
-    name:'xiaomi',
-    num:0,
-    unitPrice:5999,
-  }
-])
-const handelClick = (item:Good,type:boolean)=>{
-  if(type){
-    item.num++;
-  }else{
-    item.num--
-  }
-}
+  <div>hello world</div>
+  <button @click="handelClick">+</button>
 
-const allPrice = computed<number>(()=>{
-  return goodList.reduce((previousValue,currentValue)=>{
-    return previousValue+(currentValue.unitPrice *currentValue.num);
-  },0)
+</template>
+
+<script setup lang="ts">
+import {watch,reactive,watchEffect} from "Vue";
+const data = reactive({
+  num:0
+});
+
+// watch(()=>data.num,(newValue,oldValue)=>{
+//   console.log(newValue,oldValue);
+// })
+watchEffect(()=>{
+  console.log(data.num)
 })
 
+
+const  handelClick = ()=>{
+  data.num ++;
+}
+
+
 </script>
-
 <style scoped>
-
-.wrapper{ 
-  width: 100vw;
-  height: 100vh;
-}
-table{
-  width: 400px;
-  border: 1px solid red;
-}
-
 
 </style>
