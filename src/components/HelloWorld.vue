5852ba16719ed8516ca89b8b58d3fff22a2c66ab
“李承斌” <15031617367@163.com>
2023-05-05 16:20:48 +0800

    动画练习

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
-    <h1>this is hello world</h1>
-    
-    <Teleport :disabled="false" to="body">
-      <h2>传送门到body</h2>
-    </Teleport>
-
+    <button @click='handelClick'>click</button>
+    <transition name="text">
+      <div v-if="flag">this is text</div>
+    </transition>
   </div>
-
 </template>
-<script setup lang="ts">  
-
+<script setup lang="ts">
+import {ref} from "vue"
+const flag = ref(true);
+const handelClick = ()=>{
+  console.log("111")
+  flag.value = !flag.value
+}
 </script>
 <style scoped>
-
+.text-enter-from{
+  color: red;
+}
+.text-enter-active{
+  transition: all 5s;
+}
+.text-enter-to{
+  color: aqua;
+}
 </style>
