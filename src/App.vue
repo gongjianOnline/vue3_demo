d48ec18e42ac152958c5797d4f336ff79c9448fb
“李承斌” <15031617367@163.com>
2023-05-06 13:22:45 +0800

    添加vitejs/plugin-vue-jsx组件支持tsx语法

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div>this is app</div>
   <HelloWorld></HelloWorld>
+  <TsxComponent></TsxComponent>
 </template>
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue"
+import TsxComponent from "./app"
 </script>
 <style scoped>
 
