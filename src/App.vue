1e691adfe239a0122ee6b1a0b46f9640f1b781e0
“李承斌” <15031617367@163.com>
2023-05-06 15:36:03 +0800

    tsx语法练习创建了app.tsx

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div>this is app</div>
   <HelloWorld></HelloWorld>
-  <TsxComponent></TsxComponent>
+  <TsxComponent title="泰裤辣" @on-click="getParams"> </TsxComponent>
 </template>
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue"
 import TsxComponent from "./app"
+const getParams = (data:any)=>{
+  console.log("接收到了子组件的传参",data);
+}
 </script>
 <style scoped>
 
--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,61 @@
-
-
-export default function(){
-  return (<div>hello vue TSX</div>)
+import {defineComponent,ref,reactive} from "vue"
+// export default function(){
+//   return (<div>hello vue TSX</div>)
+// }
+/**options方式 */
+// export default defineComponent({
+//   data(){
+//     return {
+//       name:'hello world'
+//     }
+//   },
+//   render(){
+//     return (<div>{this.name}</div>)
+//   }
+// })
+/**setup方式 */
+interface Props {
+  title?:string
 }
+export default defineComponent({
+  props:{
+    title:{
+      type:String
+    }
+  },
+  emits:["on-click"],
+
+  setup(props:Props,ctx) {
+    console.log(props);
+    const index = ref("xxx");
+    const flag = ref(true);
+    const list = reactive({
+      data:[1,2,3,4,5,6,7,8,9]
+    })
+    const handleClick = ()=>{
+      flag.value = !flag.value;
+    }
+    const handelEmitClick = ()=>{
+      ctx.emit("on-click","this is childParams")
+    }
+    return ()=>(
+      <div>
+        {flag.value?<div>默认值</div>:<div>hello setup {index.value}</div>}
+        <button onClick={()=>handleClick()}>click</button>
+        <ul>  
+          {
+            list.data.map((item)=>{
+              return <li>{item}</li>
+            })
+          }
+        </ul>
+        <div data-index={index.value}>111</div>
+        <div>{props.title}</div>
+        <button onClick={()=>handelEmitClick()}>click</button>
+      </div>
+      
+    )
+  },
+})
+
+
