ea6e326390fdb17dffbcfbe2ae5cec80b228a0d7
“李承斌” <15031617367@163.com>
2023-05-09 17:40:46 +0800

    异步图片加载

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,9 @@
 <template>
   <div>this is app</div>
-  <button @click="handelClick">状态</button>
-  <div>{{textValue}}</div>
-  <HelloWorld 
-    v-model:textValue="textValue"
-    v-model="isClose"></HelloWorld>
-
+  <HelloWorld></HelloWorld>
 </template>
 <script setup lang="ts">
-import {ref} from "vue"
-import HelloWorld  from "./components/HelloWorld.vue"
-const isClose = ref(false)
-const textValue = ref("");
-const handelClick = ()=>{
-  isClose.value = !isClose.value
-}
+import HelloWorld from "./components/HelloWorld.vue"
 
 
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,47 +1,15 @@
 <template>
-  <div>this is helloWorld</div>
-  <div class="inputContainer" v-if="modelValue">
-    <div class="closeBtn"><button @click="handleClose">关闭</button></div>
-    <div>
-      <label for="input">双向数据绑定</label>
-      <input 
-        type="text" 
-        name="" 
-        id="input"
-        :value="textValue"
-        @input="handelInput($event)">
-    </div>
-  </div>
-
+  <h1>hello world</h1>
 </template>
-
 <script setup lang="ts">
-import {defineProps} from "vue";
-
-defineProps<{
-  modelValue:boolean,
-  textValue:string
-}>()
-
-const emit = defineEmits(["update:modelValue","update:textValue"])
-
-const handleClose = ()=>{
-  emit("update:modelValue",false)
-}
-
-const handelInput = (event:any)=>{
-  console.log(event.target.value)
-  emit("update:textValue",event.target.value)
-}
-
-
+const images = import.meta.glob("../assets/image/*.*");
+const imageKey = Object.keys(images);
+const inageArr:any = imageKey.map(async (item)=>{
+  let imagePath:any = await images[item]();
+  return imagePath.default;
+})
+const result = await Promise.all(inageArr);
+console.log(result)
 </script>
 <style scoped>
-.inputContainer{
-  border: 1px solid #eee;
-  padding: 20px;
-}
-.closeBtn{
-  text-align: right;
-}
 </style>
