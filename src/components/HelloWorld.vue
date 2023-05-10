6fc9b63f16fd70cc1e41c8413d4fc9c5c157622b
“李承斌” <15031617367@163.com>
2023-05-10 09:15:08 +0800

    自定义指令之图片懒加载

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,48 @@
 <template>
-  <h1>hello world</h1>
+  <div v-for="(item,index) in arr" class="container">
+    <img  :key="index" v-lazy="item"/>
+  </div>
 </template>
 <script setup lang="ts">
-const images = import.meta.glob("../assets/image/*.*");
-const imageKey = Object.keys(images);
-const inageArr:any = imageKey.map(async (item)=>{
-  let imagePath:any = await images[item]();
-  return imagePath.default;
-})
-const result = await Promise.all(inageArr);
-console.log(result)
+import type { Directive } from 'vue'
+// const images = import.meta.glob("../assets/image/*.*");
+// const imageKey = Object.keys(images);
+// const inageArr:any = imageKey.map(async (item)=>{
+//   let imagePath:any = await images[item]();
+//   return imagePath.default;
+// })
+// let result = reactive<any>({});
+// result = await Promise.all(inageArr);
+const images: Record<string, { default: string }> = import.meta.globEager('../assets/image/*.*')
+let arr = Object.values(images).map(v => v.default)
+
+
+let  vLazy:Directive = async (el,binding)=>{
+  console.log(el,binding)
+  let url = await import('../assets/vue.svg')
+  el.src = url.default;
+  let observer = new IntersectionObserver((entries) => {
+    console.log(entries);
+    if (entries[0].intersectionRatio > 0) {
+        setTimeout(() => {
+            el.src = binding.value;
+            observer.unobserve(el)
+        }, 2000)
+    }
+  })
+  observer.observe(el)
+}
+
+
+
 </script>
 <style scoped>
+.container{
+  width: 100vh;
+  height: 400px;
+}
+.container>img{
+  width: 100vh;
+}
+
 </style>
