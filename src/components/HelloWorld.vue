26c555a6dee03b0a69de0d16706e8211b97b857f
“李承斌” <15031617367@163.com>
2023-05-30 08:59:28 +0800

    练习自定义指令

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,26 +1,29 @@
 <template>
-  <div>this is HelloWorld</div>
-  <button @click="show = !show">开关{{show}}</button>
-  <button @click="handleClick">count++</button>
-  <hr>
-  <ChildComponent v-model="show" v-model:count.add="count"></ChildComponent>
-
+  <div>this is hello component</div>
+  <ChildComponent v-move="'red'"></ChildComponent>
 </template>
-
 <script setup lang="ts">
-import {ref} from "vue";
+import  {Directive} from "vue"
 import ChildComponent from "./childComponent.vue";
-const show = ref(false)
-const count = ref(0);
+// const vMove : Directive={
+//   created(...args){
+//     console.log(args);
+//     var el = args[0];
+//     var styles = args[1].value;
+//     el.style.background = styles.background;
+    
+//   }
+// }
 
-const handleClick = ()=>{
-  count.value++
+
+// 简写
+const vMove:Directive = (el,bingeing)=>{
+  console.log(bingeing);
+  el.style.background = bingeing.value;
 }
 
+
 </script>
-
-
-<style> 
-
+<style scoped>
 
 </style>
